f14cb55b0ea9edbf76ef2683fe95518047d1b6a1
parson40 <101997352+parsonjd@users.noreply.github.com>
2022-11-28 11:34:26 -0500

    added resolvers.js logic and tested server side functionality

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -12,7 +12,6 @@ module.exports = {
     if (req.headers.authorization) {
       token = token.split(" ").pop().trim();
     }
-    console.log("token", token);
 
     if (!token) {
       return req;
